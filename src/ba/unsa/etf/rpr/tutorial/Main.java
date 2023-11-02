3faad2153d9ca250ae949ef5894a8191e9272868
Zejd Krek <zejd.krek@gmail.com>
2023-11-02 13:01:48 +0100

    portanje prethodnih LV u git repo

---
+++ src/ba/unsa/etf/rpr/tutorial/Main.java
@@ -0,0 +1,29 @@
+package ba.unsa.etf.rpr.tutorial;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static int sumaCifara(int n) {
+
+        int suma = 0;
+        while(n > 0) {
+
+            suma = suma + n % 10;
+            n = n / 10;
+        }
+        return suma;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner ulaz = new Scanner(System.in);
+        System.out.println("Unesite broj n: ");
+
+        int n = ulaz.nextInt();
+
+        for (int i = 1; i <= n; ++i)
+            if (i % sumaCifara(i) == 0)
+                System.out.println(i + " " + sumaCifara(i));
+    }
+}
