cbbac529dacefe98ecf44167b85e3379b88f0415
Zejd Krek <zejd.krek@gmail.com>
2023-11-02 13:14:51 +0100

    uklanjanje pogresnih fajlova

--- src/ba/unsa/etf/rpr/tutorial/Main.java
+++
@@ -1,8 +0,0 @@
-package ba.unsa.etf.rpr.tutorial;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
--- src/ba/unsa/etf/rpr/tutorial/Uposlenik.java
+++
@@ -1,9 +0,0 @@
-package ba.unsa.etf.rpr.tutorial;
-
-public class Uposlenik extends Osoba {
-
-    public Uposlenik(String ime, String prezime) {
-
-        super(ime, prezime);
-    }
-}
