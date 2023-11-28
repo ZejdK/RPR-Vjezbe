2493e4ed6481d12fa87940f4ceedc85a4d5fceea
Zejd Krek <zejd.krek@gmail.com>
2023-11-28 21:05:20 +0100

    dodaj NeodgovarajuciProcesorException

---
+++ src/main/java/ba/unsa/etf/rpr/NeodgovarajuciProcesorException.java
@@ -0,0 +1,9 @@
+package ba.unsa.etf.rpr;
+
+public class NeodgovarajuciProcesorException extends Exception {
+
+    public NeodgovarajuciProcesorException(String errorMessage) {
+
+        super(errorMessage);
+    }
+}
