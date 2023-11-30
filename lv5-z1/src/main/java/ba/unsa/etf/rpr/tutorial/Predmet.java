a919a4081a208ada0dbf7613246741a8a1db0363
Zejd Krek <zejd.krek@gmail.com>
2023-11-30 12:03:18 +0100

    [lv5-z1] dodaj klasu Predmet

---
+++ lv5-z1/src/main/java/ba/unsa/etf/rpr/tutorial/Predmet.java
@@ -0,0 +1,33 @@
+package ba.unsa.etf.rpr.tutorial;
+
+public class Predmet {
+
+    private String naziv;
+    private String opis;
+
+
+
+    Predmet(String naziv, String opis) {
+
+        this.naziv = naziv;
+        this.opis = opis;
+    }
+
+
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public void setOpis(String opis) {
+        this.opis = opis;
+    }
+}
