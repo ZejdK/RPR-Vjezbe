117fd4b20c1666ed2a60b3b5cba1c5b5bb4e2fb4
Zejd Krek <zejd.krek@gmail.com>
2023-11-30 12:11:14 +0100

    [lv5-z1] dodaj interface Introducable koji zahtjeva implementaciju metode predstavi

---
+++ lv5-z1/src/main/java/ba/unsa/etf/rpr/tutorial/Introducable.java
@@ -0,0 +1,8 @@
+package ba.unsa.etf.rpr.tutorial;
+
+// s obzirom da je tesko naci rijec u bosanskom koja moze biti
+// ekvivalent ovoj, a da nije rogobatna, odlucio sam se za englesku rijec
+public interface Introducable {
+
+    String predstavi();
+}
