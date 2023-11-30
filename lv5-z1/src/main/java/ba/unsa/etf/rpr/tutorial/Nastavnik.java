5b68f99567a52f38cb770b8627c186345aca0ba0
Zejd Krek <zejd.krek@gmail.com>
2023-11-30 11:33:20 +0100

    Kreacija projekta

---
+++ lv5-z1/src/main/java/ba/unsa/etf/rpr/tutorial/Nastavnik.java
@@ -0,0 +1,14 @@
+package ba.unsa.etf.rpr.tutorial;
+
+public class Nastavnik extends LicneInformacije {
+
+    private String titula;
+
+    public String getTitula() {
+        return titula;
+    }
+
+    public void setTitula(String titula) {
+        this.titula = titula;
+    }
+}
---
+++ lv5-z1/src/main/java/ba/unsa/etf/rpr/tutorial/Student.java
@@ -0,0 +1,25 @@
+package ba.unsa.etf.rpr.tutorial;
+
+public class Student extends LicneInformacije {
+
+    private String godinaStudija;
+    private String brojIndexa;
+
+
+
+    public String getGodinaStudija() {
+        return godinaStudija;
+    }
+
+    public void setGodinaStudija(String godinaStudija) {
+        this.godinaStudija = godinaStudija;
+    }
+
+    public String getBrojIndexa() {
+        return brojIndexa;
+    }
+
+    public void setBrojIndexa(String brojIndexa) {
+        this.brojIndexa = brojIndexa;
+    }
+}
