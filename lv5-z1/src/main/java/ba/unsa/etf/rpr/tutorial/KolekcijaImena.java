ad247bc44fe56e7bc9b59d5636519b857b901623
Zejd Krek <zejd.krek@gmail.com>
2023-12-05 10:30:14 +0100

    [lv5-z3] dodaj klasu KolekcijaImena

---
+++ lv5-z1/src/main/java/ba/unsa/etf/rpr/tutorial/KolekcijaImena.java
@@ -0,0 +1,27 @@
+package ba.unsa.etf.rpr.tutorial;
+
+import java.util.ArrayList;
+
+public class KolekcijaImena {
+
+    private final ArrayList<String> imena;
+
+    KolekcijaImena() {
+
+        imena = new ArrayList<>();
+    }
+
+    String getNajduzeIme() throws Exception {
+
+        if (imena.size() == 0)
+            throw new Exception("Lista imena je prazna");
+
+        String najduzeIme = imena.get(0);
+
+        for (String s : imena)
+            if (s.length() > najduzeIme.length())
+                najduzeIme = s;
+
+        return najduzeIme;
+    }
+}
