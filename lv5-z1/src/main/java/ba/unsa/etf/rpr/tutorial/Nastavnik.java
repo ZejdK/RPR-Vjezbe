d04d3922ac0b2af2c91925a064803158a57d736e
Zejd Krek <zejd.krek@gmail.com>
2023-12-05 09:55:16 +0100

    [lv5-z1] dodaj metodu ocijeni u klasu Nastavnik za mogucnost ocjenjivanja nastavnika

--- lv5-z1/src/main/java/ba/unsa/etf/rpr/tutorial/Nastavnik.java
+++ lv5-z1/src/main/java/ba/unsa/etf/rpr/tutorial/Nastavnik.java
@@ -1,15 +1,17 @@
 package ba.unsa.etf.rpr.tutorial;
 
+import java.util.ArrayList;
+
 public class Nastavnik extends Osoba implements Introducable { // Nastavnik je klasa InformacijeONastavniku
 
     private String titula;
-
-
+    private final ArrayList<Ocjena> ocjene;
 
     public Nastavnik(String ime, String prezime, String titula) {
 
         super(ime, prezime);
         this.titula = titula;
+        ocjene = new ArrayList<>();
     }
 
     @Override
@@ -18,7 +20,11 @@ public class Nastavnik extends Osoba implements Introducable { // Nastavnik je k
         return this.getTitula() + super.predstavi() + " je nastavnik";
     }
 
+    public Ocjena ocijeni(Osoba ko, int ocjena) {
 
+        ocjene.add(new Ocjena(ko, ocjena));
+        return ocjene.get(ocjene.size() - 1);
+    }
 
     public String getTitula() {
         return titula;
