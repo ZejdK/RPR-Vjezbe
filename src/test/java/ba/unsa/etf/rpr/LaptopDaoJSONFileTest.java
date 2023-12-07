ecdd7c359a4899cc0031aad9d3fc6e8a09b728ed
Zejd Krek <zejd.krek@gmail.com>
2023-12-07 04:36:59 +0100

    dodaj testne klase za klase LaptopDao*

---
+++ src/test/java/ba/unsa/etf/rpr/LaptopDaoJSONFileTest.java
@@ -0,0 +1,29 @@
+package ba.unsa.etf.rpr;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+// to do zavrsiti testove
+
+class LaptopDaoJSONFileTest {
+
+    @Test
+    void dodajLaptopUListu() {
+    }
+
+    @Test
+    void dodajLaptopUFile() {
+    }
+
+    @Test
+    void getLaptop() {
+    }
+
+    @Test
+    void napuniListu() {
+    }
+
+    @Test
+    void vratiPodatkeIzDatoteke() {
+    }
+}
