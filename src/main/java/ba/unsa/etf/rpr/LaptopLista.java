78d1929964d012fe8d820f4d174b6a980caa8a3d
Zejd Krek <zejd.krek@gmail.com>
2023-12-07 03:05:13 +0100

    LaptopLista treba da implementira Serializable interface

--- src/main/java/ba/unsa/etf/rpr/LaptopLista.java
+++ src/main/java/ba/unsa/etf/rpr/LaptopLista.java
@@ -1,8 +1,9 @@
 package ba.unsa.etf.rpr;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class LaptopLista {
+public class LaptopLista implements Serializable {
 
     private ArrayList<Laptop> laptopi;
 
